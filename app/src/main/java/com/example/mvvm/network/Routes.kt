cfe9eceefd0842ac736e5173e26d2c247fb75e88
suryamudti <suryamudti0128@gmail.com>
2019-10-26 16:06:07 +0700

    update routes comment

--- app/src/main/java/com/example/mvvm/network/Routes.kt
+++ app/src/main/java/com/example/mvvm/network/Routes.kt
@@ -1,11 +1,15 @@
 package com.example.mvvm.network
 
 import com.example.mvvm.data.entity.ResponseMovies
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface Routes {
 
+    /*
+    * end_point base url
+    * */
     @GET("discover/movie")
-    fun getMovies() : retrofit2.Call<ResponseMovies>
+    fun getMovies() : Call<ResponseMovies>
 
 }
